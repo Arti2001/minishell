60ffdcf36d393fe63b8fe4e88e0b3e6145030c0c
Eugene Didenko <eugene.didenko@rabobank.nl>
2024-11-26 16:48:51 +0100

    reverse fix

--- src/execution/signals.c
+++ src/execution/signals.c
@@ -6,7 +6,7 @@
 /*   By: amysiv <amysiv@student.42.fr>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/18 17:09:25 by amysiv        #+#    #+#                 */
-/*   Updated: 2024/11/26 13:59:14 by ydidenko      ########   odam.nl         */
+/*   Updated: 2024/11/26 16:48:04 by ydidenko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ extern volatile sig_atomic_t	g_signal;
 static void	handler_int(int num)
 {
 	write(1, "\n", 2);
-	//rl_replace_line("", 0);
+	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 	g_signal = num;
