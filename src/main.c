3228c14d84dd67ea9d5097a7ce16ffa9a5ccc688
Yevhenii Didenko <eugeneddenko@gmail.com>
2024-11-28 16:43:28 +0100

    segfault fix

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amysiv <amysiv@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/08 12:56:16 by amysiv            #+#    #+#             */
-/*   Updated: 2024/11/28 13:27:44 by amysiv           ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: amysiv <amysiv@student.42.fr>                +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/08/08 12:56:16 by amysiv        #+#    #+#                 */
+/*   Updated: 2024/11/28 16:41:57 by ydidenko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,6 +149,8 @@ int main(int argc, char *argv[], char *envp[])
 	{
 		i_env = (t_i_env *)null_exit(malloc(sizeof(t_i_env)));
 		i_env->env = (t_env *)null_exit(set_env(envp));
+		if (i_env->env == NULL)
+			return (free(i_env), 1);
 		shell_lvl(i_env->env);
 		g_signal = 0;
 		input = NULL;
--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amysiv <amysiv@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/29 14:52:30 by ydidenko          #+#    #+#             */
-/*   Updated: 2024/11/26 18:01:34 by amysiv           ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   parser.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: amysiv <amysiv@student.42.fr>                +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/10/29 14:52:30 by ydidenko      #+#    #+#                 */
+/*   Updated: 2024/11/28 16:42:23 by ydidenko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ t_pars	*init_pars(char *line, t_i_env *i_env)
 	// print_tokens(tokens);
 	pars = convert_tokens(tokens);
 	if (!pars)
+	{
 		printf("Error: failed to convert tokens\n");
+		return (NULL);
+	}
 	//print_pars(pars);
 	ft_lstclear(&tokens, ((void (*))(void *)destroy_token));
 	return (pars);
