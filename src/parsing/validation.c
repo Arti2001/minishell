bfa99d5850529790ab4afbfd8d5bc9846dd85152
Yevhenii Didenko <eugeneddenko@gmail.com>
2024-12-02 17:39:43 +0100

    validation unclosed quotes

--- src/parsing/validation.c
+++ src/parsing/validation.c
@@ -6,7 +6,7 @@
 /*   By: ydidenko <ydidenko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/02 16:24:22 by ydidenko      #+#    #+#                 */
-/*   Updated: 2024/12/02 17:21:22 by ydidenko      ########   odam.nl         */
+/*   Updated: 2024/12/02 17:33:00 by ydidenko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,20 @@ static int	is_consecutive_valid(t_list *tokens)
 	return (1);
 }
 
+static int	check_quotes(t_list *tokens)
+{
+	t_token	*token;
+
+	while (tokens)
+	{
+		token = tokens->content;
+		if (token->type == UNCLOSED)
+			return (0);
+		tokens = tokens->next;
+	}
+	return (1);
+}
+
 int	is_valid_tokens(t_list *tokens)
 {
 	t_token	*token;
@@ -48,7 +62,7 @@ int	is_valid_tokens(t_list *tokens)
 		if (is_token_type_redir(*token) && token->type != REDIR_INPUT)
 			return (0);
 	}
-	if (is_consecutive_valid(tokens) == 0)
+	if (is_consecutive_valid(tokens) == 0 || check_quotes(tokens) == 0)
 		return (0);
 	return (1);
 }
